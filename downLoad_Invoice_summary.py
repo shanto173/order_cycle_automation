73bd57790bdadc5db3284cd71b66ceece35de262
shanto173 <kamrul0628@gmail.com>
2025-07-15 17:37:27 +0600

    Adding Zipper FG stock Data

--- downLoad_Invoice_summary.py
+++ downLoad_Invoice_summary.py
@@ -174,7 +174,7 @@ while True:
         # download the report
         log.info("=== download the report ===")
         wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[2]/div/div/div/div/footer/footer/button[1]"))).click() 
-        time.sleep(10)
+        time.sleep(30)
        
         # === Step 9: Confirm file downloaded ===
         
